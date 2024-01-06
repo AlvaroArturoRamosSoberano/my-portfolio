50ac5878d8714cb9b82c9a67c2832b9c008dcd2e
AlvaroArturoRamosSoberano <alvarors935@gmail.com>
2024-01-05 22:33:20 -0600

    Added Element Plus library

---
+++ src/components/buttons/LookingJobButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="pt-2 pb-2">
+        <div class="flex justify-center sm:justify-start">
+            <el-tooltip content="Looking for a job" placement="top">
+                <button
+                    class="w-10 h-9 bg-green-300 shadow-xl shadow-green-500/50 rounded-lg flex justify-center items-center cursor-default ">
+                    <!--  <SpeakerXMarkIcon class="w-5 h-5"></SpeakerXMarkIcon> -->
+                    <SpeakerWaveIcon class="w-5 h-5 animate-pulse "></SpeakerWaveIcon>
+                </button>
+            </el-tooltip>
+            <resume-button></resume-button>
+        </div>
+    </section>
+</template>
+<script setup>
+import { SpeakerXMarkIcon, SpeakerWaveIcon } from '@heroicons/vue/24/outline';
+import ResumeButton from "../buttons/ResumeButton.vue";
+
+</script>
---
+++ src/components/buttons/ResumeButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="pb-2 ps-2">
+        <div class="flex justify-center sm:justify-start">
+            <button class="w-auto h-9 bg-cyan-300 shadow-lg shadow-cyan-500/50 hover:bg-gray-400 rounded-lg flex justify-center items-center ">
+                <DocumentArrowDownIcon class="w-5 h-5 my-2 mx-2"></DocumentArrowDownIcon>
+                <a href="/src/assets/CV-Arturo_Ramos_-_Software-Engineer.pdf" class="pe-4 font-poppins text-center text-sm">Resume</a>
+            </button>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { DocumentArrowDownIcon } from '@heroicons/vue/24/solid';
+</script>
