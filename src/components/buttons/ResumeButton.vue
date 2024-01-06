e04d9d88f17a6b48ed35d18aef510a1c6e0770eb
AlvaroArturoRamosSoberano <alvarors935@gmail.com>
2024-01-06 13:46:03 -0600

    Revert "Fixed resume button"
    This reverts commit 7090f1a246cd25ccb93c86902166bb353dbe2390.

--- src/components/buttons/ResumeButton.vue
+++ src/components/buttons/ResumeButton.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="pb-2 ps-2">
         <div class="flex justify-center sm:justify-start">
-            <button class="w-auto h-9 bg-cyan-300 shadow-lg shadow-cyan-500/50 hover:bg-cyan-400 rounded-lg flex justify-center items-center ">
+            <button class="w-auto h-9 bg-cyan-300 shadow-lg shadow-cyan-500/50 hover:bg-gray-400 rounded-lg flex justify-center items-center ">
                 <DocumentArrowDownIcon class="w-5 h-5 my-2 mx-2"></DocumentArrowDownIcon>
                 <!-- <a href="/src/assets/CV_Arturo_Ramos_-_Software-Engineer.pdf" class="pe-4 font-poppins text-center text-sm">Resume</a> -->
                 <a href="/src/assets/resume.docx" class="pe-4 font-poppins text-center text-sm">Resume</a>
