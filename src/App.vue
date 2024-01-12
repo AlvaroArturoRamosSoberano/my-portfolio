43226c3f1848d0811f6b0a44fb599246c3420d1a
AlvaroArturoRamosSoberano <alvarors935@gmail.com>
2024-01-12 13:37:24 -0600

    Change on structure project

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,14 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <section class="h-auto w-auto bg-white-50 dark:bg-gray-800">
+    <div class="w-auto p-4 flex justify-start md:justify-center">
+      <div class="flex flex-col">
+        <Header />
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    </div>
+  </section>
   <RouterView />
 </template>
